d38f0922afe855a621249ea7f41784a7c9c9b03f
Doaa <Duaa_ali@outlook.sa>
2021-10-21 13:36:35 +0300

    Second commit

--- app/src/main/java/com/example/thirdpartylibrary/MainActivity.kt
+++ app/src/main/java/com/example/thirdpartylibrary/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.thirdpartylibrary
 import `is`.arontibo.library.ElasticDownloadView
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.skydoves.elasticviews.ElasticButton
 
@@ -17,19 +18,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<View>(R.id.textView) as TextView
         val elasticButton = findViewById<View>(R.id.elasticbutton) as ElasticButton
         val elasticButton1 = findViewById<View>(R.id.elasticbutton1) as ElasticButton
         val mElasticDownloadView = findViewById<View>(R.id.elastic_download_view) as ElasticDownloadView
-
+        var pro = 20F
         elasticButton.setOnClickListener {
             mElasticDownloadView.startIntro()
-            mElasticDownloadView.success()
+            mElasticDownloadView.setProgress(pro)
+            pro+=20F
+            if(pro == 100F) {
+                mElasticDownloadView.success()
+                textView.text = "Success download"
+            }
         }
 
         elasticButton1.setOnClickListener {
             mElasticDownloadView.startIntro()
             mElasticDownloadView.setProgress(40F)
             mElasticDownloadView.fail()
+            textView.text = "fail download"
         }
 
 
